91402d6afd1a64e5069436671e41637780a06492
Exhabition <lukeweerheim@hotmail.com>
2021-11-30 21:26:25 +0100

    I aplogise for your eyes and I hate myself

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,19 +3,11 @@ const closeButton = document.querySelector("#btn");
 const sideButtons = document.getElementsByClassName("sidebarButton");
 
 closeButton.addEventListener("click", () => {
-    toggleClass(sidebar, "open");
+    sidebar.classList.toggle("open");
 });
 
 for (const sideButton of sideButtons) {
     sideButton.addEventListener("click", () => {
-        toggleClass(sideButton, "active");
+        sideButton.classList.toggle("active");
     });
-}
-
-function toggleClass(element, toAdd) {
-    if (element.className.includes(toAdd)) {
-        element.className = element.className.replace(toAdd, "");
-    } else {
-        element.className += ` ${toAdd}`;
-    }
 }
